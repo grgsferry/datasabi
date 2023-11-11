04b22d80ea144653e12c9f175b8b94041961d8a9
G. Ferry <grgsferry@gmail.com>
2023-11-11 21:11:00 +0700

    first commit

---
+++ tailwind.config.js
@@ -0,0 +1,19 @@
+module.exports = {
+  content: ["./src/**/*.{html,js}"],
+  theme: {
+    extend: {
+      colors: {
+        "ds-yellow": "#FDE725",
+        "ds-green-1": "#7AD151",
+        "ds-green-2": "#22A884",
+        "ds-blue": "#2A788E",
+        "ds-purple": "#414487",
+      },
+      fontFamily: {
+        body: ['"Inter"'],
+        sans: ['"Inter"'],
+      },
+    },
+  },
+  plugins: [],
+};
