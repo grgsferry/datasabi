6d3cf8b77f0e047922667c1de4a803a7de7a6209
G. Ferry <grgsferry@gmail.com>
2023-11-19 22:25:27 +0700

    ...

--- app.js
+++ app.js
@@ -15,7 +15,7 @@ app.use("/dist", express.static(__dirname + "/dist"));
 app.use("/src", express.static(__dirname + "/src"));
 app.use("/views", express.static(__dirname + "/views"));
 
-app.set("views", path.join(__dirname, "views"));
+app.set("views", express.static(__dirname, "views"));
 app.set("view engine", "ejs");
 
 app.get("/", (req, res) => {
