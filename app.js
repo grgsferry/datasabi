8b7cf88ac405185745b1ac5605f6cefcdb5d84bd
G. Ferry <grgsferry@gmail.com>
2023-11-19 21:31:34 +0700

    ...

--- app.js
+++ app.js
@@ -11,36 +11,39 @@ const serviceRouter = express.Router({ mergeParams: true });
 
 const port = 3000;
 
-// app.use(express.static("src"));
-app.use("/dist", express.static(__dirname + "/dist"));
-app.use("/src", express.static(__dirname + "/src"));
-app.use("/views", express.static(__dirname + "/views"));
+// app.use("/dist", express.static(__dirname + "/dist"));
+// app.use("/src", express.static(__dirname + "/src"));
+// app.use("/views", express.static(__dirname + "/views"));
 
-app.set("view engine", "ejs");
+// app.set("view engine", "ejs");
+
+// app.get("/", (req, res) => {
+//   res.render("index");
+// });
+
+// app.get("/home", (req, res) => {
+//   res.render("index");
+// });
+
+// app.get("/about", (req, res) => {
+//   //   res.render("about");
+//   res.status(200).send("about");
+// });
+
+// app.get("/portfolios", (req, res) => {
+//   //   res.render("portfolios");
+//   res.status(200).send("porto");
+// });
+
+// serviceRouter.route("/:serviceId").get(function (req, res) {
+//   res.status(200).send("hello user " + req.params.serviceId);
+// });
+
+// app.use("/services", serviceRouter);
 
 app.get("/", (req, res) => {
-  res.render("index");
+  res.send("Hello");
 });
-
-app.get("/home", (req, res) => {
-  res.render("index");
-});
-
-app.get("/about", (req, res) => {
-  //   res.render("about");
-  res.status(200).send("about");
-});
-
-app.get("/portfolios", (req, res) => {
-  //   res.render("portfolios");
-  res.status(200).send("porto");
-});
-
-serviceRouter.route("/:serviceId").get(function (req, res) {
-  res.status(200).send("hello user " + req.params.serviceId);
-});
-
-app.use("/services", serviceRouter);
 
 // app.listen(port, () => {
 //   console.log(`Example app listening on port ${port}`);
